dfc8245a74903a7bf2cc2e68bb21a14bfafab09e
anotte <notte@ewti.com.br>
2023-07-25 19:01:46 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -33,11 +33,51 @@ app.get('/get_resourses', (request, response) => {
 });
 
 app.get('/get_activities', (request, response) => {
-  
+
   const sql =
     'SELECT id, atividade FROM atividades;';
 
   connection.query(sql, (error, results) => {
+    // If there is an issue with the query, output the error
+    if (error) throw error;
+    // If the account exists
+    response.send(results);
+  });
+});
+
+app.get('/get_objects', (request, response) => {
+  
+  if (!request.query.activity) {
+    response.status(400).send('activity not found');
+    return;
+  }
+
+  const activity = request.query.activity;
+
+  const sql =
+    'SELECT objetos.id, objeto FROM objetos INNER JOIN atividade_objeto ON objetos.id = atividade_objeto.id_objeto WHERE atividade_objeto.id_atividade = ?;';
+
+  connection.query(sql, [activity], (error, results) => {
+    // If there is an issue with the query, output the error
+    if (error) throw error;
+    // If the account exists
+    response.send(results);
+  });
+});
+
+app.get('/get_results', (request, response) => {
+  
+  if (!request.query.activity) {
+    response.status(400).send('activity not found');
+    return;
+  }
+
+  const activity = request.query.activity;
+
+  const sql =
+    'SELECT resultados.id, resultado FROM resultados INNER JOIN atividade_resultado ON resultados.id = atividade_resultado.id_resultado WHERE atividade_resultado.id_atividade = ?;';
+
+  connection.query(sql, [activity], (error, results) => {
     // If there is an issue with the query, output the error
     if (error) throw error;
     // If the account exists
